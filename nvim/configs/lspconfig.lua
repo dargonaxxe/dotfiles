cc1cf5f28a42be0719f27988784e3e34781ac3d1
Ilia Kuzmin <dargonaxxe@gmail.com>
2023-12-04 20:26:31 +0400

    fixed stupid configuration bug

--- nvim/configs/lspconfig.lua
+++ nvim/configs/lspconfig.lua
@@ -1,11 +1,10 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
-local elixirls = "elixirls"
 
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", elixirls }
+local servers = { "html", "cssls", "tsserver", "clangd"}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -17,6 +16,8 @@ end
 -- 
 -- lspconfig.pyright.setup { blabla}
 
-lspconfig[elixirls].setup {
+lspconfig['elixirls'].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
   cmd = { "/Users/kuzminila/.elixir-ls/release/language_server.sh" }
 }
